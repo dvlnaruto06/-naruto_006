2b6d907b7784617bfe280c1164766d11e7507f55
dvlnaruto06 <koshtiharshil006@gmail.com>
2025-04-09 12:36:19 +0530

    Update start_unix.sh

--- start_unix.sh
+++ start_unix.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-if [ -z "$UPSTREAM_REPO" ]; then
-  echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincessV2 /LazyPrincessV2
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO"
-  git clone "$UPSTREAM_REPO" /LazyPrincessV2
-fi
-
-cd LazyPrincessV2 || exit
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
